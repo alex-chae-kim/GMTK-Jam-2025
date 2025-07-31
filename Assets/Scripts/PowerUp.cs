56357eca90cd3e3291ac8d9a33d9e56630cb7613
rsiddi7777 <rsiddi@ufl.edu>
2025-07-31 19:52:16 -0400

    some example powerups which fuel card generation

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -2,14 +2,16 @@ using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 [CreateAssetMenu(fileName = "PowerUp", menuName = "Scriptable Objects/PowerUp")]
 public class PowerUp : ScriptableObject
 {
     public string name;
     public string description;
-    public Image image;
-    private int count = 0;
+    public Sprite image;
+    public int count = 0;
     public float speedBuff;
     public float jumpBuff;
+    public float lifeBuff;
     public string special;
 }
--- Assets/Scripts/PowerUpUI.cs
+++ Assets/Scripts/PowerUpUI.cs
@@ -7,19 +7,42 @@ using Unity.VisualScripting;
 
 public class PowerUpUI : MonoBehaviour
 {
-    public GameObject card1;
-    public GameObject card2;
-    public GameObject card3;
+    
+    public GameObject[] cards;
     public PowerUp[] powerUps;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject description = card1.gameObject.transform.GetChild(2).gameObject;
-        description.GetComponent<TextMeshProUGUI>().text = "pridesogged";
-        GameObject description2 = card2.gameObject.transform.GetChild(2).gameObject;
-        description2.GetComponent<TextMeshProUGUI>().text = powerUps[0].description;
+        //GameObject description = cards[0].gameObject.transform.GetChild(2).gameObject;
+        //description.GetComponent<TextMeshProUGUI>().text = "pridesogged";
+        //GameObject description2 = cards[1].gameObject.transform.GetChild(2).gameObject;
+        //description2.GetComponent<TextMeshProUGUI>().text = powerUps[0].description;
 
+        PowerUp[] currentPowers = new PowerUp[cards.Length];
+        for(int i = 0;i<cards.Length; i++) 
+        {
+            
+            int rand = Random.Range(0,powerUps.Length);
+            PowerUp chosenPower = powerUps[rand];
+            currentPowers[i] = chosenPower;
+
+            GameObject name = cards[i].gameObject.transform.GetChild(3).gameObject;
+            name.GetComponent<TextMeshProUGUI>().text = chosenPower.name;
+            print(chosenPower.name);
+            GameObject description = cards[i].gameObject.transform.GetChild(2).gameObject;
+            description.GetComponent<TextMeshProUGUI>().text = chosenPower.description;
+
+            GameObject image = cards[i].gameObject.transform.GetChild(4).gameObject;
+            image.GetComponent<Image>().sprite = chosenPower.image;
+
+            GameObject button = cards[i].gameObject.transform.GetChild(1).gameObject;
+            button.GetComponentInChildren<TextMeshProUGUI>().text = chosenPower.name;
+
+
+
+
+        }
 
     }
 
