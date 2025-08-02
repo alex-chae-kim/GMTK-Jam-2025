deaa64583de1097d6bbca40d945e69160690bb37
amandajiang259 <55763725+amandajiang259@users.noreply.github.com>
2025-08-02 15:03:11 -0400

    Pause button and menu added

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        // Toggle pause when P is pressed
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+}
