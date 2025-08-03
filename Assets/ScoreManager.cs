07758b534536a7fe9d2c418f83106bd2c5637122
rsiddi7777 <rsiddi@ufl.edu>
2025-08-03 00:25:28 -0400

    spooky leaderboard score update

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -7,10 +7,17 @@ public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI inputScore;
     [SerializeField] private TMP_InputField inputName;
+    [SerializeField] Timer timer;
+
+    private void OnEnable()
+    {
+        inputScore.text = timer.score.ToString();
+    }
 
     public UnityEvent<string, int> submitScoreEvent;
     public void SubmitScore()
     {
+        
         submitScoreEvent.Invoke(inputName.text,int.Parse(inputScore.text));
     }
 }
